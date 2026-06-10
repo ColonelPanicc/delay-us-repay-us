71ee38a2def2b8cb65211ea89e40d9e3dac274c0
Ryan Collins <ryan@ohmgeek.co.uk>
2026-06-10 21:58:09 +0100

    Change scope, expect instead of allow

--- crates/data-ingest/src/lib.rs
+++ crates/data-ingest/src/lib.rs
@@ -1,41 +1,35 @@
 #[derive(Clone)]
-#[allow(dead_code)]
-// TODO remove lint once we use this.
-pub(crate) struct RailwayStation {
+#[expect(dead_code)]
+pub struct RailwayStation {
     short_code: String,
     full_name: String,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
-// TODO remove lint once we use this.
-#[allow(dead_code)]
-pub(crate) enum DelayRepayBehaviour {
+pub enum DelayRepayBehaviour {
     DR15,
     DR30,
     Other,
 }
 
 #[derive(Clone)]
-#[allow(dead_code)]
-pub(crate) struct RailwayOperator {
+#[expect(dead_code)]
+pub struct RailwayOperator {
     short_code: String,
     full_name: String,
     delay_repay_mode: DelayRepayBehaviour,
 }
 
 #[derive(Clone)]
-#[allow(dead_code)]
-pub(crate) struct Route {
+#[expect(dead_code)]
+pub struct Route {
     starting_station: RailwayStation,
     terminus: RailwayStation,
     stops: Vec<RailwayStation>,
-    operators: RailwayOperator,
+    operator: RailwayOperator,
 }
 
-// Allow dead code for now as this doesn't have an implementation.
-// TODO: remove this once we have implemented.
-#[allow(dead_code)]
-pub(crate) trait RailwayLoader {
+pub trait RailwayLoader {
     fn load_railway_stations(self) -> Vec<RailwayStation>;
     fn load_routes(self) -> Vec<Route>;
     fn load_train_operators(self) -> Vec<RailwayOperator>;
@@ -80,15 +74,15 @@ mod tests {
         let route = Route {
             starting_station: start.clone(),
             terminus: end.clone(),
-            operators: operator,
+            operator,
             stops: vec![start.clone(), mid.clone(), end.clone()],
         };
 
         assert_eq!(route.starting_station.full_name, "London Liverpool Street");
         assert_eq!(route.terminus.full_name, "Shenfield");
-        assert_eq!(route.operators.short_code, "GA");
-        assert_eq!(route.operators.full_name, "Greater Anglia");
-        assert_eq!(route.operators.delay_repay_mode, DelayRepayBehaviour::DR15);
+        assert_eq!(route.operator.short_code, "GA");
+        assert_eq!(route.operator.full_name, "Greater Anglia");
+        assert_eq!(route.operator.delay_repay_mode, DelayRepayBehaviour::DR15);
         assert_eq!(route.stops.iter().count(), 3);
     }
 }
