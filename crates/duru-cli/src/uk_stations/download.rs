19365ba584c4b537217b44ca04f7a372800d285f
Ryan Collins <ryan@ohmgeek.co.uk>
2026-06-28 17:29:11 +0100

    Move station data to pull from NetworkRail (to help capture multiple codes)

--- crates/duru-cli/src/uk_stations/download.rs
+++ crates/duru-cli/src/uk_stations/download.rs
@@ -1,19 +1,15 @@
-use std::{
-    fs::{self, File},
-    io::Write,
-};
-
-use anyhow::Result;
-use clap::Args;
-use reqwest::blocking::get;
-
 use crate::{
     data_cache_dir::{DATA_CACHE_DIR, data_cache_file},
     uk_stations::DEFAULT_LOCAL_JSON_FILE_NAME,
 };
+use anyhow::Result;
+use clap::Args;
+use reqwest::header::CONTENT_TYPE;
+use std::env::var;
+use std::fs::{self, File};
 
-const UK_STATIONS_JSON_URL: &str =
-    "https://raw.githubusercontent.com/davwheat/uk-railway-stations/refs/heads/main/stations.json";
+const UK_STATIONS_XML_URL: &str =
+    "https://publicdatafeeds.networkrail.co.uk/ntrod/SupportingFileAuthenticate?type=CORPUS";
 
 #[derive(Args)]
 pub struct DownloadCommand {
@@ -24,12 +20,15 @@ pub struct DownloadCommand {
 
 impl DownloadCommand {
     pub fn execute(self, dry_run: bool) -> Result<()> {
-        let output_json_path =
+        let output_xml_path =
             data_cache_file(self.out.as_deref().unwrap_or(DEFAULT_LOCAL_JSON_FILE_NAME));
 
+        let username = var("DURU_NETWORK_RAIL_USERNAME")?;
+        let password = Some(var("DURU_NETWORK_RAIL_PASSWORD")?);
+
         println!(
-            "Attempting to download {UK_STATIONS_JSON_URL} to {out_file}",
-            out_file = output_json_path.display()
+            "Attempting to download {UK_STATIONS_XML_URL} to {out_file}",
+            out_file = output_xml_path.display()
         );
         if dry_run {
             return Ok(());
@@ -37,9 +36,15 @@ impl DownloadCommand {
 
         fs::create_dir_all(DATA_CACHE_DIR.as_path())?;
 
-        let file_bytes = get(UK_STATIONS_JSON_URL)?.bytes()?;
-        let mut out_file = File::create(output_json_path)?;
-        out_file.write_all(&file_bytes)?;
+        let client = reqwest::blocking::Client::new();
+        let response = client
+            .get(UK_STATIONS_XML_URL)
+            .basic_auth(username, password)
+            .header(CONTENT_TYPE, "application/json")
+            .send()?
+            .bytes()?;
+        let mut out_file = File::create(output_xml_path)?;
+        gzippy::decompress_to_writer(&response, &mut out_file)?;
 
         println!("Download complete");
         Ok(())
